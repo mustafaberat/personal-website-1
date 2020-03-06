2e4931d548bd8b32e8959b4f4a131f6d54969d0e
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-06 23:35:43 +0300

    added draft 2017-8-9

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -3,14 +3,16 @@ import Head from 'next/head'
 import Link from 'next/link'
 // import HeaderComp from "../components/headerComponent"
 var _2016 = "I started the English preparatory class in 2016 and that year, I chased a lot of tourists to practice on the street.";
-// var _2017_2020 = "In my department, i was like the dream.";
-var _2017_2020 = "I will continue...";
+var _2017 = "The first year of my department, i was like in the dream because i have wanted to analys algorithms before coding. End of the year i decide to make an internship without any ideas about things however i supposed that i was done about everything. The idea of getting my first intership was an awesome decision for me because i saw myself what i did not learn yet.";
+var _2018 = "The second year of my department, i was much more than my classmates because i have already learned HTML,CSS and JS with real project experience. In that year, i saw i have learned lots of subjects such as object oriented programming pillars and the logic of pointers. Additionally, i started to give lessons to high school students about the software. The end of the year i got crazy idea about my career. It will go to TUBITAK BILGEM to internship program again. It was impossible to apply because i was 2nd year of the department and normally they would not accept it. After getting there, i asked human resources manager, why did you accept me? She said i have been following you from the Linkedin for along time. I think I'm still surprised about it."
+var _2019 = "In the third year, I decided to take more lessons from the upper class and I'm still alive..."
+
 
 
 const About = () => (
     <div>
         <Head>
-            <title>Mustafa Berat ARU - Kişisel Web Sitesi</title>
+            <title>Mustafa Berat ARU - Personal Website</title>
             <link rel="icon" href="/m.png" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
                 integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
@@ -34,7 +36,9 @@ const About = () => (
                 <h1 className="name-surname">About Me</h1>
                 <p className="my-title about-my-title">I am <span className="about-spans">Mustafa Berat ARU</span></p>
                 <p className="my-title about-my-title">{_2016}</p>
-                <p className="my-title about-my-title">{_2017_2020}</p>
+                <p className="my-title about-my-title">{_2017}</p>
+                <p className="my-title about-my-title">{_2018}</p>
+                <p className="my-title about-my-title">{_2019}</p>
                 {/* <p className="my-title about-my-title"><span className="about-spans">I would say that i am a high potential ship with no sail</span></p> */}
                 <article className="social-media-links about-social-media-area">
                     <Link href="https://www.instagram.com/mustafaberatt/">
