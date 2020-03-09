9bc3c60ccb33174dd0f2404408b5ebc9d762dee3
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-10 00:25:02 +0300

    For now, the text is over, the next update would be the end of 2020

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 // import HeaderComp from "../components/headerComponent"
 var _2016 = "I started the English preparatory class in 2016 and that year, I chased a lot of tourists to practice on the street.";
-var _2017 = "The first year of my department, i was like in the dream because i have wanted to analys algorithms before coding. End of the year i decide to make an internship without any ideas about things however i supposed that i was done about everything. The idea of getting my first intership was an awesome decision for me because i saw myself what i did not learn yet.";
-var _2018 = "The second year of my department, i was much more than my classmates because i have already learned HTML,CSS and JS with real project experience. In that year, i saw i have learned lots of subjects such as object oriented programming pillars and the logic of pointers. Additionally, i started to give lessons to high school students about the software. The end of the year i got crazy idea about my career. It will go to TUBITAK BILGEM to internship program again. It was impossible to apply because i was 2nd year of the department and normally they would not accept it. After getting there, i asked human resources manager, why did you accept me? She said i have been following you from the Linkedin for along time. I think I'm still surprised about it."
-var _2019 = "In the third year, I decided to take more lessons from the upper class and I'm still alive..."
+var _2017 = "The first year of my department, i was like in a dream because i have wanted to analyze algorithms before start coding. End of the year i decided to make an internship without any idea about the field work however i supposed that I did everything enough. I think the idea of getting my first intership was an awesome decision because I had the chance to observe what I didnt learn yet.";
+var _2018 = "The second year of my department, I was knowing much more than my classmates because i have already learned HTML,CSS and JS with real project experience. In that year, i realize that i have learned lots of subjects such as object oriented programming pillars and the logic of pointers and much more. Additionally, I became software tutor for high school students. At the end of the year I decided to take a crazy step about my career that I will go to TUBITAK BILGEM for internship program. It was impossible to apply because i was 2nd year of the department and normally they would not accept it but they did. After getting there, i asked human resources manager, why did you accept me? She said i have been following you from the Linkedin for along time. I think I'm still surprised about it."
+var _2019 = "In the third year, I decided to take more lessons from the upper class. ";
 
 
 
@@ -38,7 +38,7 @@ const About = () => (
                 <p className="my-title about-my-title">{_2016}</p>
                 <p className="my-title about-my-title">{_2017}</p>
                 <p className="my-title about-my-title">{_2018}</p>
-                <p className="my-title about-my-title">{_2019}</p>
+                <p className="my-title about-my-title">{_2019} <span className="about-spans">My next goal is to work and study at the same time at the end of 2020</span></p>
                 {/* <p className="my-title about-my-title"><span className="about-spans">I would say that i am a high potential ship with no sail</span></p> */}
                 <article className="social-media-links about-social-media-area">
                     <Link href="https://www.instagram.com/mustafaberatt/">
