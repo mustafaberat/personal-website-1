edd3ac6d80dca3fb68edff9f03eb8d1fe5c01cc3
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-20 13:45:43 +0300

    add mailforhome component for showing mail into homepage and its css

---
+++ components/MailForHomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const MailForHomePage = () => {
+    return (
+        <a href="mailto:mustafa.berat.aru@gmail.com" className="mailforhomepage">mustafa.berat.aru et gmail dat com</a>
+    );
+}
+
+export default MailForHomePage
