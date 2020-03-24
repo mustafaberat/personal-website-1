b241aedb623b63090cc6a9b54dc2976c15f9e75c
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-24 14:37:08 +0300

    png problem fixed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Welcome to my page
 
-![screenshot of the home page](https://github.com/mustafaberat/personal-website-1/blob/master/public/img/output.png)
+![screenshot of the home page](https://github.com/mustafaberat/personal-website-1/blob/master/public/img/output.PNG)
--- components/ThingsComp.js
+++ components/ThingsComp.js
@@ -6,8 +6,8 @@ const Things = () => {
         <div className="container">
             <h1 className="name-surname">Things</h1>
             <div className="things">
-                <ThingsCard text='Personal Website' imageSource='/img/output.png' path='/' />
-                <ThingsCard text='XOX Game' imageSource='/img/xox-game-output.png' path='https://xox.now.sh/' />
+                <ThingsCard text='Personal Website' imageSource='img/output.PNG' path='/' />
+                <ThingsCard text='XOX Game' imageSource='img/xox-game-output.png' path='https://xox.now.sh/' />
                 {/* <ThingsCard text='DIV BUTTON' imageSource='img/output.png' path='#' /> */}
                 {/* <ThingsCard text='DIV BUTTON' imageSource='m-darkBlue.png' path='#' /> */}
                 {/* <ThingsCard text='DIV BUTTON' imageSource='m-blue.png' path='#' /> */}
