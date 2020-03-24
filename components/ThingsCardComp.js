8692e0af05fcc14f20143b416b12d10937ee0805
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-24 14:34:23 +0300

    change path problem, PNG to png

--- components/ThingsCardComp.js
+++ components/ThingsCardComp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link'
 
-function ThingsCard({ text, imageSource, path = '#' }) {
+function ThingsCard({ text, imageSource, path = '' }) {
     // export default function ThingsCard({ text, path = "/" }) {
     return (
         <div className="thingsCard">
