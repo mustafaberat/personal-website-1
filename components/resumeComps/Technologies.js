da4091fd2c7fd8652f995a82ec210427f7543c52
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-24 22:21:59 +0300

    technologies draft is done

--- components/resumeComps/Technologies.js
+++ components/resumeComps/Technologies.js
@@ -3,7 +3,40 @@ import React from 'react';
 const Technologies = () => {
     return (
         <div className="container">
-            <h1>Technologies page</h1>
+            <h1 className="name-surname">Resume</h1>
+            <h1 className="technologies-title">Technologies</h1>
+            <div className="technologies-container">
+                <div className="technologies-point">HTML5 / CSS3 / SCSS</div>
+                <div className="technologies-lvl">
+                    <div className="technologies-lvl-in100">-</div>
+                </div>
+
+
+                <div className="technologies-point">JS / React / Vue</div>
+                <div className="technologies-lvl">
+                    <div className="technologies-lvl-in100">-</div>
+                </div>
+
+
+                <div className="technologies-point">Java / C# / C++</div>
+                <div className="technologies-lvl">
+                    <div className="technologies-lvl-in80">-</div>
+                </div>
+
+
+                <div className="technologies-point">Python / PostgreSQL / MySQL</div>
+                <div className="technologies-lvl">
+                    <div className="technologies-lvl-in75">-</div>
+                </div>
+
+
+                <div className="technologies-point">PHP / Adobe Photoshop</div>
+                <div className="technologies-lvl">
+                    <div className="technologies-lvl-in75">-</div>
+                </div>
+
+
+            </div>
         </div>
     );
 }
