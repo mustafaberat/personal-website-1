503dbd8423db6b50ec9c8bbf66185edfff43574b
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-25 17:48:04 +0300

    resume-technologies is done

--- components/resumeComps/Technologies.js
+++ components/resumeComps/Technologies.js
@@ -8,31 +8,31 @@ const Technologies = () => {
             <div className="technologies-container">
                 <div className="technologies-point">HTML5 / CSS3 / SCSS</div>
                 <div className="technologies-lvl">
-                    <div className="technologies-lvl-in100">-</div>
+                    <div className="technologies-lvl-in100"></div>
                 </div>
 
 
                 <div className="technologies-point">JS / React / Vue</div>
                 <div className="technologies-lvl">
-                    <div className="technologies-lvl-in100">-</div>
+                    <div className="technologies-lvl-in100"></div>
                 </div>
 
 
                 <div className="technologies-point">Java / C# / C++</div>
                 <div className="technologies-lvl">
-                    <div className="technologies-lvl-in80">-</div>
+                    <div className="technologies-lvl-in80"></div>
                 </div>
 
 
                 <div className="technologies-point">Python / PostgreSQL / MySQL</div>
                 <div className="technologies-lvl">
-                    <div className="technologies-lvl-in75">-</div>
+                    <div className="technologies-lvl-in75"></div>
                 </div>
 
 
                 <div className="technologies-point">PHP / Adobe Photoshop</div>
                 <div className="technologies-lvl">
-                    <div className="technologies-lvl-in75">-</div>
+                    <div className="technologies-lvl-in75"></div>
                 </div>
 
 
