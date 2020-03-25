f1a84c813ac28837cd006dfabb49b3707acfe066
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-25 18:53:12 +0300

    workexperience draft is done

--- components/resumeComps/WorkExperience.js
+++ components/resumeComps/WorkExperience.js
@@ -3,7 +3,16 @@ import React from 'react';
 const WorkExperience = () => {
     return (
         <div className="container">
-            <h1>WorkExperience page</h1>
+            <section className="workexperience-container">
+                <h1 className="workexperience-title">Work Experience</h1>
+                <h3 className="work-title">Work Title</h3>
+                <div className="work-subtitles">
+                    <h3 className="work-companay">componay</h3>
+                    <h3 className="work-location">, location</h3>
+                    <h3 className="work-date">date</h3>
+                </div>
+                <p className="work-details">details</p>
+            </section>
         </div>
     );
 }
