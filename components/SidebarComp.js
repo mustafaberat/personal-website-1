31e6643116c84df3c110e0551a4eaad1fe4dacde
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-27 20:22:03 +0300

    add sidebar new icon when it is opened

--- components/SidebarComp.js
+++ components/SidebarComp.js
@@ -13,15 +13,20 @@ class SidebarComp extends React.Component {
                 <button className="sidebar-menu-btn"
                     onClick={() => {
                         let element = document.getElementById("header-buttons")
+                        let element2 = document.getElementById("sidebariconid")
                         if (this.state.openStatues === false) {
                             element.style.display = "flex";
+                            element2.classList.add("fa-times")
+                            element2.classList.remove("fa-bars")
                             this.state.openStatues = true
                         } else if (this.state.openStatues === true) {
                             element.style.display = "none";
+                            element2.classList.add("fa-bars")
+                            element2.classList.remove("fa-times")
                             this.state.openStatues = false
                         }
                     }}>
-                    <i className="fas fa-bars"></i>
+                    <i id="sidebariconid" className="fas fa-bars"></i>
                 </button>
             </nav>
         );
