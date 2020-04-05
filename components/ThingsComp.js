ad2a7de3048944041c3f96dc2cf28f06a107a642
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-04-05 20:24:59 +0300

    adding a new thing project - piano

--- components/ThingsComp.js
+++ components/ThingsComp.js
@@ -11,9 +11,11 @@ const Things = () => {
                 <ThingsCard text='XOX Game' imageSource='img/xox-game-output.png' path='https://xox.now.sh/' />
                 <ThingsCard text='Personal Website' imageSource='img/output.PNG' path='/' />
                 <ThingsCard text='Minesweeper' imageSource='img/minesweeper-game-output.PNG' path='https://mayin-tarlasi.now.sh/' />
+                <ThingsCard text='Piano' imageSource='img/piano.PNG' path='https://piano-beta.now.sh/' />
                 {/* <ThingsCard text='DIV BUTTON' imageSource='img/output.png' path='#' /> */}
                 {/* <ThingsCard text='DIV BUTTON' imageSource='m-darkBlue.png' path='#' /> */}
                 {/* <ThingsCard text='DIV BUTTON' imageSource='m-blue.png' path='#' /> */}
+                <div style={{ marginBottom: "30px" }}></div>
             </div>
         </div>
     );
