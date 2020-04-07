1fd093bcdce53afe77546a2f74113290f51632df
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-04-07 23:28:37 +0300

    optimized thingsCard image source

--- components/ThingsComp.js
+++ components/ThingsComp.js
@@ -8,13 +8,26 @@ const Things = () => {
             <MyHead />
             <h1 className="name-surname">Things</h1>
             <div className="things">
-                <ThingsCard text='XOX Game' imageSource='img/xox-game-output.png' path='https://xox.now.sh/' />
-                <ThingsCard text='Personal Website' imageSource='img/output.PNG' path='/' />
-                <ThingsCard text='Minesweeper' imageSource='img/minesweeper-game-output.PNG' path='https://mayin-tarlasi.now.sh/' />
-                <ThingsCard text='Piano' imageSource='img/piano.PNG' path='https://piano-beta.now.sh/' />
-                {/* <ThingsCard text='DIV BUTTON' imageSource='img/output.png' path='#' /> */}
-                {/* <ThingsCard text='DIV BUTTON' imageSource='m-darkBlue.png' path='#' /> */}
-                {/* <ThingsCard text='DIV BUTTON' imageSource='m-blue.png' path='#' /> */}
+                <ThingsCard
+                    text='XOX Game'
+                    imageSource='https://raw.githubusercontent.com/mustafaberat/XOX-Game-by-Vue/master/src/img/Screenshot.PNG'
+                    path='https://xox.now.sh/' />
+                <ThingsCard
+                    text='Personal Website'
+                    imageSource='img/output.PNG'
+                    path='/' />
+                <ThingsCard
+                    text='Minesweeper'
+                    imageSource='https://raw.githubusercontent.com/mustafaberat/MayinTarlasi-Game-by-Vue/master/src/output/screenshot.PNG'
+                    path='https://mayin-tarlasi.now.sh/' />
+                <ThingsCard
+                    text='Piano'
+                    imageSource='https://raw.githubusercontent.com/mustafaberat/piano/master/public/screenshot.PNG'
+                    path='https://piano-beta.now.sh/' />
+                <ThingsCard
+                    text='Flappy Bee'
+                    imageSource='https://raw.githubusercontent.com/mustafaberat/Flappy-bee/master/public/VizvizGame.png'
+                    path='https://vizviz.now.sh/' />
                 <div style={{ marginBottom: "30px" }}></div>
             </div>
         </div>
