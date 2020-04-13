79a5854744ef986a99530aa78eed4d01e36c70f8
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-04-13 19:26:03 +0300

    adding theme color

--- components/MyHead.js
+++ components/MyHead.js
@@ -6,6 +6,7 @@ const MyHead = () => {
             <title>Mustafa Berat ARU - Personal Website</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/m-blue.png" />
+            <meta name="theme-color" content="#03061c" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
                 integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
         </Head>
