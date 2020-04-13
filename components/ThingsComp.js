b31c72a0fe2d80b96bfabeb313c00091f355af3d
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-04-13 19:37:10 +0300

    seperated things with titles

--- components/ThingsComp.js
+++ components/ThingsComp.js
@@ -7,6 +7,9 @@ const Things = () => {
         <div className="container">
             <MyHead />
             <h1 className="name-surname">Things</h1>
+            <div className="things-titlediv">
+                <h1 className="workexperience-title">Websites</h1>
+            </div>
             <div className="things">
                 <ThingsCard
                     text='Covid19'
@@ -17,26 +20,33 @@ const Things = () => {
                     imageSource='https://raw.githubusercontent.com/mustafaberat/binarygap-js/master/src/assets/screenshot.PNG'
                     path='https://github.com/mustafaberat/binarygap-js' />
                 <ThingsCard
-                    text='Minesweeper'
-                    imageSource='https://raw.githubusercontent.com/mustafaberat/MayinTarlasi-Game-by-Vue/master/src/output/screenshot.PNG'
-                    path='https://mayin-tarlasi.now.sh/' />
+                    text='Personal Website'
+                    imageSource='img/output.PNG'
+                    path='/' />
+                <div style={{ marginBottom: "20px" }}></div>
+            </div>
+            <div className="things-titlediv">
+                <h1 className="workexperience-title">Games</h1>
+
+            </div>
+            <div className="things">
                 <ThingsCard
                     text='Flappy Bee'
                     imageSource='https://raw.githubusercontent.com/mustafaberat/Flappy-bee/master/public/VizvizGame.png'
                     path='https://vizviz.now.sh/' />
                 <ThingsCard
+                    text='Minesweeper'
+                    imageSource='https://raw.githubusercontent.com/mustafaberat/MayinTarlasi-Game-by-Vue/master/src/output/screenshot.PNG'
+                    path='https://mayin-tarlasi.now.sh/' />
+                <ThingsCard
                     text='XOX Game'
                     imageSource='https://raw.githubusercontent.com/mustafaberat/XOX-Game-by-Vue/master/src/img/Screenshot.PNG'
                     path='https://xox.now.sh/' />
                 <ThingsCard
-                    text='Personal Website'
-                    imageSource='img/output.PNG'
-                    path='/' />
-                <ThingsCard
                     text='Piano'
                     imageSource='https://raw.githubusercontent.com/mustafaberat/piano/master/public/screenshot.PNG'
                     path='https://piano-beta.now.sh/' />
-                <div style={{ marginBottom: "30px" }}></div>
+                <div style={{ marginBottom: "20px" }}></div>
             </div>
         </div>
     );
