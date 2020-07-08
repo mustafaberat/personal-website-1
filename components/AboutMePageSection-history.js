fbaa9bf1ba097dd7c882819956e775a2e67bd36e
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-07-09 01:56:32 +0300

    update about me text

--- components/AboutMePageSection-history.js
+++ components/AboutMePageSection-history.js
@@ -2,22 +2,25 @@ import React from 'react';
 
 import SocialMedia from '../components/SocialMedia'
 
-var _2016 = "I started the English preparatory class in 2016 and that year, I chased a lot of tourists to practice on the street.";
-var _2017 = "The first year of my department, i was like in a dream because i have wanted to analyze algorithms before start coding. End of the year i decided to make an internship without any idea about the field work however i supposed that I did everything enough. I think the idea of getting my first intership was an awesome decision because I had the chance to observe what I didnt learn yet.";
-var _2018 = "The second year of my department, I was knowing much more than my classmates because i have already learned HTML,CSS and JS with real project experience. In that year, i realize that i have learned lots of subjects such as object oriented programming pillars and the logic of pointers and much more. Additionally, I became software tutor for high school students. At the end of the year I decided to take a crazy step about my career that I will go to TUBITAK BILGEM for internship program. It was impossible to apply because i was 2nd year of the department and normally they would not accept it but they did. After getting there, i asked human resources manager, why did you accept me? She said i have been following you from the Linkedin for along time. I think I'm still surprised about it."
-var _2019 = "In the third year, I decided to take more lessons from the upper class. ";
+var _2016 = "I started the English preparatory class in 2016 and that year, I chased a lot of tourists to practice on the street. Some of them tried to escape from my slow talk but I got better day by day.";
+var _2017 = "The first year of my department was like a dream because I saw a lot of code and it was great. I found a part-time job on web applications at my school. Then I learned that I love taking lessons and also working in a company at the same time. At the end of the year, I decided to do an internship without any idea about field work, but I thought I had done everything enough. The idea of ​​getting my first internship was a great decision because I had the chance to observe what I had not yet learned.";
+var _2018 = "The second year of my department, I was knowing much more than my classmates because i have already learned HTML,CSS and JS with real project experience. In that year, i realize that i have learned lots of subjects such as object oriented programming pillars and the logic of pointers and much more. Additionally, I took my second part-time job and on Sunday became a software teacher for high school students. At the end of the year I decided to take a crazy step about my career that I will go to TUBITAK BILGEM for internship program. It was impossible to apply because i was 2nd year of the department and normally they would not accept it but they did. After getting there, i asked human resources manager, why did you accept me? She said i have been following you from the Linkedin for along time. I think I'm still surprised about it."
+var _2019 = "In the third year, I decided to take more lessons from the upper class to gain free time in the 4th grade. I quit my first part-time job because I took everything I could get from it, but I focused on being an instructor and gave many lessons to the children. As an intern at the end of the year I found an enormous defense company to serve my flag. ";
 
 const AboutPageSection = () => {
     return (
         <section className="section">
             <div className="container">
                 <h1 className="name-surname">About Me</h1>
-                <p className="my-title about-my-title">I am <span className="about-spans">Mustafa Berat ARU</span></p>
+                <p className="my-title about-my-title">Hi! I am <span className="about-spans">Mustafa Berat ARU</span></p>
                 <p className="my-title about-my-title">{_2016}</p>
                 <p className="my-title about-my-title">{_2017}</p>
                 <p className="my-title about-my-title">{_2018}</p>
-                <p className="my-title about-my-title">{_2019} <span className="about-spans">My next goal is to work and study at the same time at the end of 2020</span></p>
-                {/* <p className="my-title about-my-title"><span className="about-spans">I would say that i am a high potential ship with no sail</span></p> */}
+
+                <p className="my-title about-my-title">{_2019}
+                    <span className="about-spans">My next goal is to work and study at the same time at the end of 2020</span>
+                </p>
+
                 <SocialMedia />
             </div>
         </section>
