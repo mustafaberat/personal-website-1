11bd82a3de1d1167ea30d33733bcdd68de2a9171
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2024-04-18 09:01:19 +0300

    Update current company

--- components/HomePageSection-namesurname.js
+++ components/HomePageSection-namesurname.js
@@ -8,7 +8,7 @@ const HomePageSection = () => {
     <section className="section">
       <div className="container">
         <h1 className="name-surname">MUSTAFA BERAT ARU</h1>
-        <p className="my-title">Software Engineer at <span className="my-title-company">Trendyol</span></p>
+        <p className="my-title">Senior Software Engineer at <span className="my-title-company">Getir</span></p>
         <SocialMedia />
         <MailForHomePage />
       </div>
